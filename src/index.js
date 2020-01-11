12e049d26cb93c5f4969ea755871384ba70e2036
github username <saramschwarz@gmail.com>
2020-01-11 17:12:31 -0500

    store creation

--- src/index.js
+++ src/index.js
@@ -3,13 +3,27 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
 import { createStore } from 'redux';
 import allReducer from './reducers/index';
 import { Provider } from 'react-redux';
 
+//STORE -> globalized state
 const store = createStore(allReducer,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
+
+//ACTION i.e. increment, describes what you want to do
+//type is the name of the action e.g. type: INCREMENT
+const increment = () => {
+  return {
+    type: INCREMENT
+  }
+}
+
+//REDUCER describes how actions transform state to the next state
+
+//DISPATCH excecutes action
 
 
 ReactDOM.render(
