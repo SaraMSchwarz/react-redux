ac3fa6b8e87dfc5ae0ce79a3cf80535dd6b7c406
github username <saramschwarz@gmail.com>
2020-01-11 18:09:25 -0500

    counter reducer

--- src/index.js
+++ src/index.js
@@ -29,6 +29,17 @@ const decrement = () => {
 
 //REDUCER describes how actions transform state to the next state
 
+//based on the action name/type, it's going to do something
+
+const counter = (state = 0, action) => {
+  switch(action.type) {
+    case "INCREMENT":
+      return state + 1;
+    case "DECREMENT":
+      return state - 1;
+  }
+}
+
 //DISPATCH excecutes action
 
 
